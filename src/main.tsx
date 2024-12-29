815f66e2630872a807efea785f0577b228d80165
LWR8 <ismiselee@gmail.com>
2024-12-29 02:27:17 +0000

    change values to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import './Styles/styles.css'
 import App from './App.tsx'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 )
